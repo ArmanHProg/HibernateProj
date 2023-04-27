eba5968bf495f1ef8ebfa6b7969246966b258b4f
Arman Hakhverdyan <arman.hakhverdyan@bk.ru>
2023-04-27 20:46:22 +0400

    management project

---
+++ src/main/java/org/managment/model/Passenger.java
@@ -0,0 +1,103 @@
+package org.managment.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "passengers")
+public class Passenger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "address_id")
+    private String address_id;
+
+
+    /**
+     * Default constructor;
+     */
+    public Passenger() {
+
+    }
+
+    /**
+     * @param name
+     * @param phone
+     * @param address_id
+     */
+    public Passenger(String name, String phone, String address_id) {
+        this.name = name;
+        this.phone = phone;
+        this.address_id = address_id;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    public String getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(String address_id) {
+        this.address_id = address_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address_id='" + address_id + '\'' +
+                '}';
+    }
+}
